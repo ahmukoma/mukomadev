f33df0ddb4686a8d4c7ea552fcc42d0ba5442730
ahmukoma <42545076+ahmukoma@users.noreply.github.com>
2018-08-21 10:19:09 -0400

    updated meta tags

--- index.php
+++ index.php
@@ -23,9 +23,9 @@ $run = mysqli_query($conn, $insert);
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		
 		<meta charset="utf-8">
+		<meta name="description" content="Abdi Mukoma is an up-and-coming creative thinking professional Web Developer mainly experienced in creating, designing and implementing both static and dynamic web technologies.">
+		<meta name="keywords" content="abdi mukoma, mukoma, abdikadir mukoma, front end web developer, web developer">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="">
-		<meta name="keywords" content="">
 		
 		<script src="js/jquery-3.2.1.js"></script>
 	</head>
